0e30dd6403a97ef3330b1bf0b774a72db64972f8
bbeaird <charles.amoako.128@gmail.com>
2013-10-28 21:46:52 -0700

    Added routes to recall.rb, added delete and edit erb files. Still doesn't work though...get error when trying to shotgun recall.rb

--- recall.rb
+++ recall.rb
@@ -1,4 +1,4 @@
-# require 'rubygems'
+require 'rubygems'
 require 'sqlite3'
 require 'sinatra'
 require 'data_mapper'
@@ -15,7 +15,7 @@ class Note
 	property :updated_at, DateTime
 end
 
-DataMapper.finalize.auto_upgrade!
+# DataMapper.finalize.auto_upgrade!
 
 get '/' do
 	# "Hello world!"
@@ -33,5 +33,38 @@ post '/' do
 	redirect '/'
 end
 
+get '/:id' do
+	@note = Note.get params[:id]
+	@title = "Edit note ##{params[:id]}"
+	erb :edit
+end
 
+put '/:id' do
+  n = Note.get params[:id]
+  n.content = params[:content]
+  n.complete = params[:complete] ? 1 : 0
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
+
+get '/:id/delete' do
+  @note = Note.get params[:id]
+  @title = "Confirm deletion of note ##{params[:id]}"
+  erb :delete
+end
+
+delete '/:id' do
+  n = Note.get params[:id]
+  n.destroy
+  redirect '/'
+end
+
+get '/:id/complete' do
+  n = Note.get params[:id]
+  n.complete = n.complete ? 0 : 1 # flip it
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
 
